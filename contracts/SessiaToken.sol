75bec107d2f4c046b50a20fd5b588b5373aefb7a
Nikita Kuznetsov <n.kuznetsov@iconic.vc>
2017-12-06 18:46:42 +0300

    make token not transferable

--- contracts/SessiaToken.sol
+++ contracts/SessiaToken.sol
@@ -6,29 +6,14 @@ import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 contract SessiaToken is MintableToken {
 
     string public constant name = 'Sessia Kickers';
-    string public constant symbol = 'KICK';
+    string public constant symbol = 'pKICK';
     uint8 public constant decimals = 18;
 
-    mapping(address=>bool) whitelist;
-
-    event Burn(address indexed from, uint256 value);
-    event AllowTransfer(address from);
-
-    modifier canTransfer() {
-        require(mintingFinished || whitelist[msg.sender]);
-        _;
+    function transferFrom(address from, address to, uint256 value) returns (bool) {
+        revert();
     }
 
-    function allowTransfer(address from) onlyOwner {
-        AllowTransfer(from);
-        whitelist[from] = true;
-    }
-
-    function transferFrom(address from, address to, uint256 value) canTransfer returns (bool) {
-        return super.transferFrom(from, to, value);
-    }
-
-    function transfer(address to, uint256 value) canTransfer returns (bool) {
-        return super.transfer(to, value);
+    function transfer(address to, uint256 value) returns (bool) {
+        revert();
     }
 }
